83f2f485233ad467af13da08354898cd938248ba
XNOR8 <das.srinanda2002@gmail.com>
2022-04-08 21:07:25 +0530

    created initial structure for login page

--- graphpassclient/src/Components/LoginPage/LoginPage.jsx
+++ graphpassclient/src/Components/LoginPage/LoginPage.jsx
@@ -3,7 +3,20 @@ import "./LoginPage.css"
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>LoginPage
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+
+      <div>
+      <label htmlFor="username">User ID</label>
+      <input type="text" name="username" id="username" />
+      </div>
+
+      <button type="submit">Login</button>
+      
+    </div>
   )
 }
 
