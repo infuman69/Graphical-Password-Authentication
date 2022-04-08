b02425dddb0bf6cd19e5fb048bac256bc883b41b
NNBSTRIKERS <betal07rick@gmail.com>
2022-04-08 21:27:22 +0530

    sign up inital struct

--- graphpassclient/src/Components/LoginPage/LoginPage.jsx
+++ graphpassclient/src/Components/LoginPage/LoginPage.jsx
@@ -4,6 +4,8 @@ import "./LoginPage.css"
 const LoginPage = () => {
   return (
     <div>LoginPage
+
+      
       <div>
         <label htmlFor="username">User ID</label>
         <input type="text" name="username" id="username" />
--- graphpassclient/src/Components/SignupPage/SignupPage.jsx
+++ graphpassclient/src/Components/SignupPage/SignupPage.jsx
@@ -3,7 +3,30 @@ import "./SignupPage.css"
 
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <div>SignupPage
+        
+        <div>
+        <label htmlFor="firstname">First Name</label>
+        <input type="text" name="firstname" id="firstname" />
+      </div>
+
+      <div>
+        <label htmlFor="lastname">Last Name</label>
+        <input type="text" name="lastname" id="lasttname" />
+      </div>
+
+      <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+      </div>
+
+      <div>
+          <label htmlFor="username">User ID</label>
+          <input type="text" name="username" id="username" />
+      </div>
+
+
+    </div>
   )
 }
 
