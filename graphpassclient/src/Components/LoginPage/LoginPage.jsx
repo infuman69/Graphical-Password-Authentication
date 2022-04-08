1c2b005f92b098b31565c634a18e93a3df3f87b5
XNOR8 <das.srinanda2002@gmail.com>
2022-04-08 21:16:56 +0530

    updated login page

--- graphpassclient/src/Components/LoginPage/LoginPage.jsx
+++ graphpassclient/src/Components/LoginPage/LoginPage.jsx
@@ -5,17 +5,17 @@ const LoginPage = () => {
   return (
     <div>LoginPage
       <div>
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
+        <label htmlFor="username">User ID</label>
+        <input type="text" name="username" id="username" />
       </div>
 
       <div>
-      <label htmlFor="username">User ID</label>
-      <input type="text" name="username" id="username" />
+      <label htmlFor="email">Email</label>
+      <input type="text" name="email" id="email" />
       </div>
 
       <button type="submit">Login</button>
-      
+
     </div>
   )
 }
